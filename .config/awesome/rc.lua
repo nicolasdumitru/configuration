8331663198c4c77707acc4f24ffb1d4fc5ef8d66
nick <nicolas.dumitru.nd.nd@gmail.com>
2023-04-27 20:07:00 +0300

    incomplete cleanup

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -242,10 +242,6 @@ root.buttons(gears.table.join(
 
 -- Defaults:
 --[[
-globalkeys = gears.table.join(
-    awful.key({ modkey,           }, "s",      hotkeys_popup.show_help,
-              {description="show help", group="awesome"}),
-    awful.key({ modkey,           }, "Left",   awful.tag.viewprev,
               {description = "view previous", group = "tag"}),
     awful.key({ modkey,           }, "Right",  awful.tag.viewnext,
               {description = "view next", group = "tag"}),
